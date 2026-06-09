2324c71b28ba03b3a4fbc4c28f3e0da50537e90d
smartcraze <dev.surajv@gmail.com>
2026-06-10 00:49:52 +0530

    feat : channel services is done

--- apps/channel-service/index.ts
+++ apps/channel-service/index.ts
@@ -1,1 +1,32 @@
-console.log("Hello via Bun!");
+import express from "express";
+import { z } from "zod";
+import { simulateLifecycle } from "./simulate";
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 3001;
+
+const sendPayloadSchema = z.object({
+    communicationId: z.string(),
+    message: z.string(),
+    channel: z.string(),
+});
+
+
+app.post("/send", (req, res) => {
+    try {
+        const payload = sendPayloadSchema.parse(req.body);
+
+        res.status(200).json({ success: true, message: "Accepted for delivery" });
+
+        void simulateLifecycle(payload.communicationId);
+    } catch (error) {
+        res.status(400).json({ success: false, error: "Invalid payload" });
+        console.log("[Invalid Payload]", error);
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Channel Service mock running on port ${PORT}`);
+});
